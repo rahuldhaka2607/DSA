15d69e9d27d6f2403370f725ede857c082339633
rahuldhaka2607 <rahuldhaka2607@gmail.com>
2026-07-31 10:12:07 +0530

    dsa

---
+++ function/3_error_forward_declation.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    sayHello();
+    return 0;
+}
+void sayHello(){
+    cout<<"Hello";
+}
---
+++ function/4_forward_declation.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+void sayHello();
+
+int main(){
+    sayHello();
+    return 0;
+}
+void sayHello(){
+    cout<<"Hello";
+}
