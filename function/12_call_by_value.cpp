e621786742873189a1de81fc5f9409b7a2edb4e1
rahuldhaka2607 <rahuldhaka2607@gmail.com>
2026-07-31 10:15:54 +0530

    dsa

---
+++ function/12_call_by_value.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// Function receives a copy
+void increment(int num)
+{
+    num++;
+    cout << "Inside function: " << num << endl;
+}
+
+int main()
+{
+    int number = 5;
+
+    increment(number);
+
+    cout << "Outside function: " << number << endl;
+
+    return 0;
+}
