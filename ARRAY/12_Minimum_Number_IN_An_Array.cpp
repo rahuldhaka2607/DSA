cc88c2c22cfec8bb901786e5af6d4d329e6f748a
rahuldhaka2607 <rahuldhaka2607@gmail.com>
2026-08-11 17:11:41 +0530

    11 Maximum Number IN An Array and 12 minimum Number IN An Array

--- ARRAY/12_Minimum_Number_IN_An_Array.cpp
+++ ARRAY/12_Minimum_Number_IN_An_Array.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int main(){
     int arr[]={10,25,7,89,45};
     int n=sizeof(arr)/sizeof(arr[0]);
-    int max=arr[0];
+    int min=arr[0];
           //or
-    // we also use int max=INT_MIN for this need to include #include<iostream>;      
+    // we also use int min=INT_MAX for this need to include #include<iostream>;      
     for(int i=1;i<n;i++){
-        if(arr[i]>max){
-            max=arr[i];
+        if(arr[i]<min){
+            min=arr[i];
         }
     }
-    cout<<"Maximum Element="<<max;
+    cout<<"Minimum Element="<<min;
     return 0;
 }
