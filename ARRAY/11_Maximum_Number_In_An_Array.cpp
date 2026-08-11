a00d0d69ab100fb707933748010e4b6bfd30bce4
rahuldhaka2607 <rahuldhaka2607@gmail.com>
2026-08-11 17:05:03 +0530

    11 Maximum Number IN An Array

---
+++ ARRAY/11_Maximum_Number_In_An_Array.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[]={10,25,7,89,45};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int max=arr[0];
+          //or
+    // we also use int max=INT_MAX for this need to include #include<iostream>;      
+    for(int i=1;i<n;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+    cout<<"Maximum Element="<<max;
+    return 0;
+}
