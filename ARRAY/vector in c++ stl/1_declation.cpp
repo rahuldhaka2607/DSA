6a54a04305c20dba194cb7fce3760320c5a56f99
rahuldhaka2607 <rahuldhaka2607@gmail.com>
2026-08-16 21:16:19 +0530

    vactor declaration extra information

--- ARRAY/vector in c++ stl/1_declation.cpp
+++ ARRAY/vector in c++ stl/1_declation.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main(){
     vector<int> arr;
-    //it double its size after array;
+    //it double its size after array is full;
+    //it defalut size is zero
     return 0;
 }
