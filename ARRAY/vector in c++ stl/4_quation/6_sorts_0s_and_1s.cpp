0a8ba157a14a808baccbcb1b9e8f9e9f63769db0
rahuldhaka2607 <rahuldhaka2607@gmail.com>
2026-08-22 15:37:41 +0530

    sort 0s and 1s

---
+++ ARRAY/vector in c++ stl/4_quation/6_sorts_0s_and_1s.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int> arr{0,1,0,1,1,0,0,1};
+
+    int start=0;
+    int end=arr.size()-1;
+    int i=0;
+    while(i<=end){
+        if(arr[i]==0){
+            //swap from left
+            swap(arr[start],arr[i]);
+            i++;
+            start++;
+        }
+        else{
+            //swap from right
+            swap(arr[i],arr[end]);
+            end--;
+        }
+    }
+    for(int x:arr){
+        cout<<x<<" ";
+    }
+    return 0;
+}
