54d399ba6bbe5387dcf317e53cb8d9ff32bea39c
rahuldhaka2607 <rahuldhaka2607@gmail.com>
2026-08-30 17:04:46 +0530

    missing elemnts from an array with duplicates

---
+++ ARRAY/Simple Array/15_missing_elements_from_an_array_with_duplicates.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+void findMissing(int a[], int n)
+{
+    // Mark numbers that are present
+    for (int i = 0; i < n; i++)
+    {
+        int index = abs(a[i]);
+
+        if (a[index - 1] > 0)
+        {
+            a[index - 1] *= -1;
+        }
+    }
+
+    // Find missing numbers
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] > 0)
+        {
+            cout << i + 1 << " ";
+        }
+    }
+}
+
+int main()
+{
+    int a[] = {1, 3, 5, 3, 4};
+
+    int n = sizeof(a) / sizeof(a[0]);
+
+    findMissing(a, n);
+
+    return 0;
+}
