b7ad63d42fa7a4010c505902a2c7a61a50738c22
rahuldhaka2607 <rahuldhaka2607@gmail.com>
2026-09-01 20:43:44 +0530

    p no 50 fixing

--- LEET CODE/4_P_NO_50_pow_.cpp
+++ LEET CODE/4_P_NO_50_pow_.cpp
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 class Solution {
 public:
     double myPow(double x, int n) {
         return pow(x, n);
     }
 };kdkdk
+=======
+class Solution {
+public:
+    double myPow(double x, int n) {
+        return pow(x, n);
+    }
+};
+>>>>>>> 6d9d4d5 (p no 50 fixing)
